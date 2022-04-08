84c46b8fd65442cb6bebaf27375f4d1e245a26d5
effervescent000 <kislio01@gmail.com>
2022-04-08 12:13:03 -0400

    added useEffect to check if a user is logged in

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import axios from "axios";
+import Cookies from "js-cookie";
 
 import "bootstrap/scss/bootstrap.scss";
 import "react-datepicker/dist/react-datepicker.css";
@@ -18,6 +20,23 @@ function App() {
     const toggleLogIn = () => {
         setLoggedIn(!loggedIn);
     };
+
+    useEffect(() => {
+        if (!loggedIn) {
+            axios
+                .get(`${process.env.REACT_APP_DOMAIN}/auth/check`, {
+                    withCredentials: true,
+                    headers: { "X-CSRF-TOKEN": Cookies.get("csrf_access_token") },
+                })
+                .then((response) => {
+                    if (Object.keys(response.data).length > 0) {
+                        toggleLogIn();
+                        setUser(response.data);
+                    }
+                })
+                .catch((error) => console.log(error.response));
+        }
+    }, []);
 
     return (
         <Router>
