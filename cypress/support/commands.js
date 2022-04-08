4722fe30c5faffff97e6240b47caebdf4de5da76
effervescent000 <kislio01@gmail.com>
2022-04-08 10:35:34 -0400

    login form tested

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -10,3 +10,15 @@ Cypress.Commands.add("signup", (username, password, confirmPassword = password) 
     cy.get("button[type=submit]").click();
     cy.wait(1000);
 });
+
+Cypress.Commands.add("login", (username, password) => {
+    cy.get(".menu").click();
+    cy.wait(500);
+    cy.get("a[href='/auth/login'").click();
+    cy.get(".btn-close").click();
+    cy.wait(250);
+    cy.get("input[name=username]").type(username);
+    cy.get("input[name=password]").type(password);
+    cy.get("button[type=submit]").click();
+    cy.wait(1000);
+});
