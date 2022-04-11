6363c9c4741c6a9670ae2d1257a5b9e393598c32
effervescent000 <kislio01@gmail.com>
2022-04-11 07:52:19 -0400

    added pageSize select to view pets page

--- src/components/pets/view-pets/view-pet-page.js
+++ src/components/pets/view-pets/view-pet-page.js
@@ -4,13 +4,14 @@ import Cookies from "js-cookie";
 
 import PetCard from "./pet-card";
 import Pagination from "../../pagination";
+import SelectField from "../../form-components/select-field";
 
 const ViewPetsPage = (props) => {
     const [allPets, setAllPets] = useState([]);
     const [filteredPets, setFilteredPets] = useState([]);
     const [displayedPets, setDisplayedPets] = useState([]);
     const [page, setPage] = useState(0);
-    const [petsPerPage, setPetsPerPage] = useState(5);
+    const [petsPerPage, setPetsPerPage] = useState(3);
 
     useEffect(() => {
         axios
@@ -30,20 +31,34 @@ const ViewPetsPage = (props) => {
 
     useEffect(() => {
         setDisplayedPets(filteredPets.slice(petsPerPage * page, petsPerPage * page + petsPerPage));
-    }, [filteredPets]);
+    }, [filteredPets, petsPerPage]);
 
     const onPageChange = (newPage) => {
         setPage(newPage);
     };
 
+    const handleChange = (event) => {
+        if (event.target.name === "page-size-select") {
+            setPetsPerPage(event.target.value);
+        }
+    };
+
     return (
         <div className="view-pets-wrapper">
-            <Pagination
-                currentPage={page}
-                pageSize={petsPerPage}
-                onPageChange={onPageChange}
-                totalCount={filteredPets.length}
-            />
+            <div className="pagination-wrapper">
+                <select name="page-size-select" onChange={handleChange} value={petsPerPage}>
+                    <option value={3}>3</option>
+                    <option value={6}>6</option>
+                    <option value={12}>12</option>
+                </select>
+                <Pagination
+                    currentPage={page}
+                    pageSize={petsPerPage}
+                    onPageChange={onPageChange}
+                    totalCount={filteredPets.length}
+                />
+            </div>
+
             {displayedPets.map((pet, index) => (
                 <PetCard key={pet.id} pet={pet} index={index} />
             ))}
--- src/components/pets/view-pets/view-pet-page.test.js
+++ src/components/pets/view-pets/view-pet-page.test.js
@@ -7,7 +7,7 @@ import { petArray } from "../../../testutils";
 
 jest.mock("axios");
 
-describe("View pets loading testing", () => {
+describe("View pets testing", () => {
     beforeEach(async () => {
         axios.get.mockResolvedValue({
             data: petArray,
@@ -16,6 +16,16 @@ describe("View pets loading testing", () => {
     });
     test("displays pets properly", async () => {
         expect(await screen.findByText(petArray[0].name)).toBeInTheDocument();
-        expect(await screen.findByText(petArray[4].name)).toBeInTheDocument();
+        expect(await screen.findByText(petArray[2].name)).toBeInTheDocument();
+    });
+    test("pageSize select works", async () => {
+        const pageSizeSelect = screen.getByRole("combobox");
+        expect(pageSizeSelect).toHaveDisplayValue("3");
+        expect(screen.getAllByText(/#\d/)).toHaveLength(3);
+
+        // change pageSize selector to 6 and test it
+        userEvent.selectOptions(pageSizeSelect, screen.getByRole("option", { name: 6 }));
+        await waitFor(() => expect(pageSizeSelect).toHaveDisplayValue(6));
+        await waitFor(() => expect(screen.getAllByText(/#\d/)).toHaveLength(6));
     });
 });
